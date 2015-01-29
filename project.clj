88357507452451c66c8de4f8931c6f57e00c92cd
David Horizon <davidself@163.com>
2015-01-29 21:05:52 +0800

    add project.clj

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject yan "0.1.0"
+            :url "https://github.com/davidself/yan"
+            :license {:name "Apache License, Version 2.0"
+                      :url "http://www.apache.org/licenses/LICENSE-2.0"}
+            :eval-in-leiningen true
+            :dependencies[[org.clojure/clojure "1.6.0"]
+                          [org.clojure/clojurescript "0.0-2740"]
+                          [org.omcljs/om "0.8.7"]
+                          [racehub/om-bootstrap "0.3.4"]
+                          [com.datomic/datomic-free "0.9.5130"]])
